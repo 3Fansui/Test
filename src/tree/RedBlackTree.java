1bdb7737a44512cbf05fb192af5a7846cdc1bb80
wu <1295683880@qq.com>
2025-02-10 16:39:20 +0800

    Initial commit

---
+++ src/tree/RedBlackTree.java
@@ -0,0 +1,517 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static tree.RedBlackTree.Color.RED;
+import static tree.RedBlackTree.Color.BLACK;
+
+/**
+ * <h3>红黑树</h3>
+ */
+public class RedBlackTree {
+
+    enum Color {
+        RED, BLACK;
+    }
+
+    Node root;
+
+    static class Node {
+        int key;
+        Object value;
+        Node left;
+        Node right;
+        Node parent;        // 父节点
+        Color color = RED;  // 颜色
+
+        public Node(int key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Node(int key) {
+            this.key = key;
+        }
+
+        public Node(int key, Color color) {
+            this.key = key;
+            this.color = color;
+        }
+
+        public Node(int key, Color color, Node left, Node right) {
+            this.key = key;
+            this.color = color;
+            this.left = left;
+            this.right = right;
+            if (left != null) {
+                left.parent = this;
+            }
+            if (right != null) {
+                right.parent = this;
+            }
+        }
+
+        // 是否是左孩子
+        boolean isLeftChild() {
+            return parent != null && parent.left == this;
+        }
+
+        // 叔叔
+        Node uncle() {
+            if (parent == null || parent.parent == null) {
+                return null;
+            }
+            if (parent.isLeftChild()) {
+                return parent.parent.right;
+            } else {
+                return parent.parent.left;
+            }
+        }
+
+        // 兄弟
+        Node sibling() {
+            if (parent == null) {
+                return null;
+            }
+            if (this.isLeftChild()) {
+                return parent.right;
+            } else {
+                return parent.left;
+            }
+        }
+    }
+
+    // 判断红
+    boolean isRed(Node node) {
+        return node != null && node.color == RED;
+    }
+
+    // 判断黑
+    boolean isBlack(Node node) {
+//        return !isRed(node);
+        return node == null || node.color == BLACK;
+    }
+
+    // 右旋 1. parent 的处理 2. 旋转后新根的父子关系
+    private void rightRotate(Node pink) {
+        Node parent = pink.parent;
+        Node yellow = pink.left;
+        Node green = yellow.right;
+        if (green != null) {
+            green.parent = pink;
+        }
+        yellow.right = pink;
+        yellow.parent = parent;
+        pink.left = green;
+        pink.parent = yellow;
+        if (parent == null) {
+            root = yellow;
+        } else if (parent.left == pink) {
+            parent.left = yellow;
+        } else {
+            parent.right = yellow;
+        }
+    }
+
+    // 左旋
+    private void leftRotate(Node pink) {
+        Node parent = pink.parent;
+        Node yellow = pink.right;
+        Node green = yellow.left;
+        if (green != null) {
+            green.parent = pink;
+        }
+        yellow.left = pink;
+        yellow.parent = parent;
+        pink.right = green;
+        pink.parent = yellow;
+        if (parent == null) {
+            root = yellow;
+        } else if (parent.left == pink) {
+            parent.left = yellow;
+        } else {
+            parent.right = yellow;
+        }
+    }
+
+    /**
+     * 新增或更新
+     * <br>
+     * 正常增、遇到红红不平衡进行调整
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public void put(int key, Object value) {
+        Node p = root;
+        Node parent = null;
+        while (p != null) {
+            parent = p;
+            if (key < p.key) {
+                p = p.left;
+            } else if (p.key < key) {
+                p = p.right;
+            } else {
+                p.value = value; // 更新
+                return;
+            }
+        }
+        Node inserted = new Node(key, value);
+        if (parent == null) {
+            root = inserted;
+        } else if (key < parent.key) {
+            parent.left = inserted;
+            inserted.parent = parent;
+        } else {
+            parent.right = inserted;
+            inserted.parent = parent;
+        }
+        fixRedRed(inserted);
+    }
+
+    void fixRedRed(Node x) {
+        // case 1 插入节点是根节点，变黑即可
+        if (x == root) {
+            x.color = BLACK;
+            return;
+        }
+        // case 2 插入节点父亲是黑色，无需调整
+        if (isBlack(x.parent)) {
+            return;
+        }
+        /* case 3 当红红相邻，叔叔为红时
+            需要将父亲、叔叔变黑、祖父变红，然后对祖父做递归处理
+        */
+        Node parent = x.parent;
+        Node uncle = x.uncle();
+        Node grandparent = parent.parent;
+        if (isRed(uncle)) {
+            parent.color = BLACK;
+            uncle.color = BLACK;
+            grandparent.color = RED;
+            fixRedRed(grandparent);
+            return;
+        }
+
+        // case 4 当红红相邻，叔叔为黑时
+        if (parent.isLeftChild() && x.isLeftChild()) { // LL
+            parent.color = BLACK;
+            grandparent.color = RED;
+            rightRotate(grandparent);
+        } else if (parent.isLeftChild()) { // LR
+            leftRotate(parent);
+            x.color = BLACK;
+            grandparent.color = RED;
+            rightRotate(grandparent);
+        } else if (!x.isLeftChild()) { // RR
+            parent.color = BLACK;
+            grandparent.color = RED;
+            leftRotate(grandparent);
+        } else { // RL
+            rightRotate(parent);
+            x.color = BLACK;
+            grandparent.color = RED;
+            leftRotate(grandparent);
+        }
+    }
+
+    /**
+     * 删除
+     * <br>
+     * 正常删、会用到李代桃僵技巧、遇到黑黑不平衡进行调整
+     *
+     * @param key 键
+     */
+    public void remove(int key) {
+        Node deleted = find(key);
+        if (deleted == null) {
+            return;
+        }
+        doRemove(deleted);
+    }
+
+    public boolean contains(int key) {
+        return find(key) != null;
+    }
+
+    // 查找删除节点
+    private Node find(int key) {
+        Node p = root;
+        while (p != null) {
+            if (key < p.key) {
+                p = p.left;
+            } else if (p.key < key) {
+                p = p.right;
+            } else {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    // 查找剩余节点
+    private Node findReplaced(Node deleted) {
+        if (deleted.left == null && deleted.right == null) {
+            return null;
+        }
+        if (deleted.left == null) {
+            return deleted.right;
+        }
+        if (deleted.right == null) {
+            return deleted.left;
+        }
+        Node s = deleted.right;
+        while (s.left != null) {
+            s = s.left;
+        }
+        return s;
+    }
+
+    // 处理双黑 (case3、case4、case5)
+    private void fixDoubleBlack(Node x) {
+        if (x == root) {
+            return;
+        }
+        Node parent = x.parent;
+        Node sibling = x.sibling();
+        // case 3 兄弟节点是红色
+        if (isRed(sibling)) {
+            if (x.isLeftChild()) {
+                leftRotate(parent);
+            } else {
+                rightRotate(parent);
+            }
+            parent.color = RED;
+            sibling.color = BLACK;
+            fixDoubleBlack(x);
+            return;
+        }
+        if (sibling != null) {
+            // case 4 兄弟是黑色, 两个侄子也是黑色
+            if (isBlack(sibling.left) && isBlack(sibling.right)) {
+                sibling.color = RED;
+                if (isRed(parent)) {
+                    parent.color = BLACK;
+                } else {
+                    fixDoubleBlack(parent);
+                }
+            }
+            // case 5 兄弟是黑色, 侄子有红色
+            else {
+                // LL
+                if (sibling.isLeftChild() && isRed(sibling.left)) {
+                    rightRotate(parent);
+                    sibling.left.color = BLACK;
+                    sibling.color = parent.color;
+                }
+                // LR
+                else if (sibling.isLeftChild() && isRed(sibling.right)) {
+                    sibling.right.color = parent.color;
+                    leftRotate(sibling);
+                    rightRotate(parent);
+                }
+                // RL
+                else if (!sibling.isLeftChild() && isRed(sibling.left)) {
+                    sibling.left.color = parent.color;
+                    rightRotate(sibling);
+                    leftRotate(parent);
+                }
+                // RR
+                else {
+                    leftRotate(parent);
+                    sibling.right.color = BLACK;
+                    sibling.color = parent.color;
+                }
+                parent.color = BLACK;
+            }
+        } else {
+            // @TODO 实际也不会出现，触发双黑后，兄弟节点不会为 null
+            fixDoubleBlack(parent);
+        }
+    }
+
+    private void doRemove(Node deleted) {
+        Node replaced = findReplaced(deleted);
+        Node parent = deleted.parent;
+        // 没有孩子
+        if (replaced == null) {
+            // case 1 删除的是根节点
+            if (deleted == root) {
+                root = null;
+            } else {
+                if (isBlack(deleted)) {
+                    // 复杂调整
+                    fixDoubleBlack(deleted);
+                } else {
+                    // 红色叶子, 无需任何处理
+                }
+                if (deleted.isLeftChild()) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+                deleted.parent = null;
+            }
+            return;
+        }
+        // 有一个孩子
+        if (deleted.left == null || deleted.right == null) {
+            // case 1 删除的是根节点
+            if (deleted == root) {
+                root.key = replaced.key;
+                root.value = replaced.value;
+                root.left = root.right = null;
+            } else {
+                if (deleted.isLeftChild()) {
+                    parent.left = replaced;
+                } else {
+                    parent.right = replaced;
+                }
+                replaced.parent = parent;
+                deleted.left = deleted.right = deleted.parent = null;
+                if (isBlack(deleted) && isBlack(replaced)) {
+                    // 复杂处理 @TODO 实际不会有这种情况 因为只有一个孩子时 被删除节点是黑色 那么剩余节点只能是红色不会触发双黑
+                    fixDoubleBlack(replaced);
+                } else {
+                    // case 2 删除是黑，剩下是红
+                    replaced.color = BLACK;
+                }
+            }
+            return;
+        }
+        // case 0 有两个孩子 => 有一个孩子 或 没有孩子
+        int t = deleted.key;
+        deleted.key = replaced.key;
+        replaced.key = t;
+
+        Object v = deleted.value;
+        deleted.value = replaced.value;
+        replaced.value = v;
+        doRemove(replaced);
+    }
+
+    public void printTree() {
+        List<List<String>> lines = new ArrayList<>();
+        List<Node> level = new ArrayList<>();
+        List<Node> next = new ArrayList<>();
+
+        level.add(root);
+        int nn = 1;
+        int widest = 0;
+
+        while (nn != 0) {
+            List<String> line = new ArrayList<>();
+
+            nn = 0;
+
+            for (Node n : level) {
+                if (n == null) {
+                    line.add(null);
+
+                    next.add(null);
+                    next.add(null);
+                } else {
+                    String aa = n.key + "(" + (n.color == RED ? "R" : "B") + ")";
+                    line.add(aa);
+                    if (aa.length() > widest) widest = aa.length();
+
+                    next.add(n.left);
+                    next.add(n.right);
+
+                    if (n.left != null) nn++;
+                    if (n.right != null) nn++;
+                }
+            }
+
+            if (widest % 2 == 1) widest++;
+
+            lines.add(line);
+
+            List<Node> tmp = level;
+            level = next;
+            next = tmp;
+            next.clear();
+        }
+
+        int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
+        for (int i = 0; i < lines.size(); i++) {
+            List<String> line = lines.get(i);
+            int hpw = (int) Math.floor(perpiece / 2f) - 1;
+
+            if (i > 0) {
+                for (int j = 0; j < line.size(); j++) {
+
+
+                    char c = ' ';
+                    if (j % 2 == 1) {
+                        if (line.get(j - 1) != null) {
+                            c = (line.get(j) != null) ? '┴' : '┘';
+                        } else {
+                            if (j < line.size() && line.get(j) != null) c = '└';
+                        }
+                    }
+                    System.out.print(c);
+
+
+                    if (line.get(j) == null) {
+                        for (int k = 0; k < perpiece - 1; k++) {
+                            System.out.print(" ");
+                        }
+                    } else {
+
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? " " : "─");
+                        }
+                        System.out.print(j % 2 == 0 ? "┌" : "┐");
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? "─" : " ");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+
+
+            for (int j = 0; j < line.size(); j++) {
+
+                String f = line.get(j);
+                if (f == null) f = "";
+                int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
+                int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
+
+
+                for (int k = 0; k < gap1; k++) {
+                    System.out.print(" ");
+                }
+                System.out.print(f);
+                for (int k = 0; k < gap2; k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+            perpiece /= 2;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+
+        // 插入节点
+        tree.put(10, "Value10");
+        tree.put(20, "Value20");
+        tree.put(30, "Value30");
+        tree.put(15, "Value15");
+        tree.put(25, "Value25");
+        tree.put(5, "Value5");
+        tree.put(1, "Value1");
+
+        tree.printTree();
+        System.out.println();
+
+        tree.remove(20);
+        tree.printTree();
+    }
+}
